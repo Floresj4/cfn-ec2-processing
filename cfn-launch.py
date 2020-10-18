08d100ca06d9fdd434265b0fab89b130ea6fc8b1
Jason <floresj4@gmail.com>
2020-10-18 11:35:32 -0400

    load params for client call

--- cfn-launch.py
+++ cfn-launch.py
@@ -8,10 +8,21 @@ call.  The template.yaml should be tested through the
 wizard to minimize errors encountered through
 the client call
 '''
-def load_template_body(path: str):
-    logger.info(f'Loading cloudformation template from {path}')
-    with open(path, 'r') as yaml_file:
+def load_template_body(template_body_path: str):
+    logger.info(f'Loading Cloudformation template from {template_body_path}')
+
+    # load as string data because AWS !Ref syntax is not standard yaml
+    with open(template_body_path, 'r') as yaml_file:
         return ''.join(yaml_file.readlines())
+
+'''
+Load yaml parameters to be substituted into the template
+via the client call
+'''
+def load_parameters(parameters_path: str):
+    logger.info(f'Loading Cloudformation parameters from {parameters_path}')
+    with open(parameters_path, 'r') as yaml_file:
+        return yaml.load(yaml_file, Loader = yaml.FullLoader)
 
 # initialize a logger
 FORMAT = '[%(levelname)s]:%(asctime)s %(message)s'
@@ -21,13 +32,15 @@ logger = logging.getLogger('cfn-logger')
 # setup arguments for the script
 parser = argparse.ArgumentParser(description = 'Cloudformation launch script')
 parser.add_argument('--stack_name', help = 'The name of the stack being created')
-parser.add_argument('--template_body', default = './template.yaml', help = 'The template URI used in stack creation')
+parser.add_argument('--template_body', default = './cloudformation/template.yaml', help = 'The path to the template used in the client call')
+parser.add_argument('--template_parameters', default = './cloudformation/parameters.yaml', help = 'The path to the parameters used in the client call.  The AWS CFN Wizard would prompt for these values.')
 args = parser.parse_args()
 
 #collect arguments here
 stack_name = args.stack_name.capitalize()
 template_body = args.template_body
 template_body_str = load_template_body(template_body)
+template_parameters = load_parameters(args.template_parameters)
 
 logger.debug(f'Stringified template body\n{template_body_str}')
 
