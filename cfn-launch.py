d20b19025212abe66ec9289366f49be5898f06eb
Jason <floresj4@gmail.com>
2020-10-18 11:20:48 -0400

    template loading

--- cfn-launch.py
+++ cfn-launch.py
@@ -1,5 +1,17 @@
 import boto3
 import argparse, logging
+import yaml, json
+
+'''
+load the template body used in the CFN client 
+call.  The template.yaml should be tested through the
+wizard to minimize errors encountered through
+the client call
+'''
+def load_template_body(path: str):
+    logger.info(f'Loading cloudformation template from {path}')
+    with open(path, 'r') as yaml_file:
+        return ''.join(yaml_file.readlines())
 
 # initialize a logger
 FORMAT = '[%(levelname)s]:%(asctime)s %(message)s'
@@ -9,13 +21,20 @@ logger = logging.getLogger('cfn-logger')
 # setup arguments for the script
 parser = argparse.ArgumentParser(description = 'Cloudformation launch script')
 parser.add_argument('--stack_name', help = 'The name of the stack being created')
+parser.add_argument('--template_body', default = './template.yaml', help = 'The template URI used in stack creation')
 args = parser.parse_args()
 
 #collect arguments here
-stack_name = args.stack_name
+stack_name = args.stack_name.capitalize()
+template_body = args.template_body
+template_body_str = load_template_body(template_body)
 
-# begin stack creation
-logger.info(f'Creating stack {stack_name}')
+logger.debug(f'Stringified template body\n{template_body_str}')
 
 # initialize CFN client here
+logger.info(f'Creating stack named {stack_name}...')
 cfn = boto3.client('cloudformation')
+# cfn.create_stack(
+#     StackName = stack_name,
+#     TemplateBody = template_body
+# )
