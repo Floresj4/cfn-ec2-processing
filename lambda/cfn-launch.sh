2d13aa7baff4210617de1dd7637063c833e1bd61
Jason <floresj4@gmail.com>
2020-10-21 20:08:01 -0400

    use arg for workspace path

--- lambda/cfn-launch.sh
+++ lambda/cfn-launch.sh
@@ -3,7 +3,8 @@ set -e
 
 # install all requirements and copy the python script
 pip3 install -r "$1/requirements.txt" -t /usr/share/lambda-temp
-cp "$1/cfn-launch.py" /usr/share/lambda-tempcp
+cp "$1/cfn-launch.py" /usr/share/lambda-temp
 
 # create the lamda archive to the bind mount
-zip -r /usr/share/workspace/lambda/cfn-launch.zip /usr/share/workspace/lambda
+cd /usr/share/lambda-temp
+zip -r $1/lambda-out/cfn-launch.zip .
