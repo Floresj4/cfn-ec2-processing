5f9d739e2589bbd7de49bb12a7a5338bbfc35469
Jason <floresj4@gmail.com>
2020-10-26 22:37:04 -0400

    build to /lambda/build

--- lambda/cfn-launch.sh
+++ lambda/cfn-launch.sh
@@ -2,9 +2,9 @@
 set -e
 
 # install all requirements and copy the python script
-pip3 install -r "$1/requirements.txt" -t /usr/share/lambda-temp
-cp $1/lambda/*.py /usr/share/lambda-temp/
+pip3 install -r "$1/lambda/requirements.txt" -t /usr/share/lambda-temp
+cp $1/lambda/src/*.py /usr/share/lambda-temp/
 
 # create the lamda archive to the bind mount
 cd /usr/share/lambda-temp
-zip -r $1/lambda-out/cfn-launch.zip .
+zip -r $1/lambda/build/cfn-launch.zip .
