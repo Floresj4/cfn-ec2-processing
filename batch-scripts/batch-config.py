e669c85ad8f8178ad7e73b5abf35a8136996ec13
Jason <floresj4@gmail.com>
2020-11-25 22:59:05 -0500

    iterate to upload

--- batch-scripts/batch-config.py
+++ batch-scripts/batch-config.py
@@ -17,11 +17,24 @@ def initialize_logger(name: str = __name__):
 logger = initialize_logger()
 
 '''
+Deploy configuration.  Iterate properties and upload them to
+a Parameter Store path.
+'''
+def deploy_configuration(props: Properties, nmspce: str):
+    # add prefix slash if necessary
+    nmspce = nmspce if nmspce[0] == '/' else '/' + nmspce
+
+    for k,v in props.items():
+        param_path = f'{nmspce}/{k}'
+        logger.debug(f'Uploading param by path -> {param_path}')
+
+'''
 load arguments required by application
 '''
 def init_arguments():
     parser = argparse.ArgumentParser(description = 'Migrate Configuration')
     parser.add_argument('filepath', help = 'Properties file to upload.')
+    parser.add_argument('namespace', help = 'Namespace path used in property deployment {namepsace/{prop_name}={prop_value}')
     return parser.parse_args()
 
 '''
@@ -32,12 +45,13 @@ def load_properties(args):
     with open(args.filepath, 'rb') as prop_file:
         props.load(prop_file, 'utf-8')
 
+    # log it clean
     dashes = 10*'-'
     logger.info(f'{dashes}Properties loaded successfully.{dashes}')
     for k, v in props.items():
         logger.info(f'{k} = {v}')
-
     logger.info(dashes * 2)
+
     return props
 
 '''
@@ -50,9 +64,12 @@ if __name__ == '__main__':
 
     try:
         logger.info('Migrating configuration...')
-        
+
         args = init_arguments()
+        nmspce = args.namespace
+
         properties = load_properties(args)
+        deploy_configuration(properties, nmspce)
 
         logger.info('Migration completed successfully.')
 
