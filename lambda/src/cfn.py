258e3538fdba6a47f4813ffbfcf37486c0b903a9
Jason <floresj4@gmail.com>
2020-12-23 22:48:02 -0500

    move into directory for execution

--- lambda/src/cfn.py
+++ lambda/src/cfn.py
@@ -126,13 +126,13 @@ def get_user_data(bucket_path, name, namespace):
 
     userdata = ['#!/bin/sh',
         f'yum update -y',
-        f'mkdir -p {batch_dir} && touch {batch_dir}/namespace',
+        f'mkdir -p {batch_dir} && cd {batch_dir} && touch {batch_dir}/namespace',
         f'echo namespace={namespace} >> {batch_dir}/namespace',
         f'aws s3 cp s3://{bucket_path}/batch-init.py {batch_dir}',
         f'yum install -y python3',
         f'python3 -m pip install boto3',
         f'python3 -m pip install requests',
-        f'python3 {batch_dir}/batch-init.py &'
+        f'python3 batch-init.py &'
     ]
 
     stringified = '\n'.join(userdata)
